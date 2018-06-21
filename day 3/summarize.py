2e59b68ed295aff3d60541e22f4750c580f3b808
tianchi.sun@cloudfortdata.com <tianchi.sun@cloudfortdata.com>
2018-06-21 09:19:02 +0800

    day 4

---
+++ day 3/summarize.py
@@ -0,0 +1,18 @@
+
+a =  '中文'.encode("utf-8")
+print(a)
+
+def func_name(arg1,arg2,arg3,*args,**kwargs):
+    pass
+
+# 位置参数 关键参数
+#print(func_name(7,8,1,77,88,11,name=stc))
+#          7,8,1,(77,88,11),{'name':'stc'}
+
+#局部变量 全局变量
+
+'''
+f=open('test.txt','wb',encoding="ytf-8")   r,w,a r+,w+,a+  rb,wb,ab
+f.
+f.close
+'''
